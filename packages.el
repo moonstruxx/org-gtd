ea2a40c3638355c5179d65a5136670584c17b5f3
thb1hi <bjoern.thorwirth@de.bosch.com>
2019-10-23 16:40:44 +0200

    rever org-refile to old behaviour

--- packages.el
+++ packages.el
@@ -67,7 +67,7 @@
       )
     )
   )
- 
+
 
 (defun gtd/init-boxquote()
   (use-package boxquote
@@ -342,21 +342,21 @@
   (setq org-refile-use-outline-path t)
 
   ;; ;; Targets complete directly with IDO
-  ;; (setq org-outline-path-complete-in-steps nil)
+  (setq org-outline-path-complete-in-steps nil)
 
   ;; Allow refile to create parent tasks with confirmation
   (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
   ;;   ;; ;; Use IDO for both buffer and file completion and ido-everywhere to t
-  ;;   ;; (setq org-completion-use-ido t)
-  ;;   ;; (setq ido-everywhere t)
-  ;;   ;; (setq ido-max-directory-size 100000)
-  ;;   ;; (ido-mode (quote both))
+  (setq org-completion-use-ido t)
+  (setq ido-everywhere t)
+  (setq ido-max-directory-size 100000)
+  (ido-mode (quote both))
   ;;   ;; ;; Use the current window when visiting files and buffers with ido
-  ;;   ;; (setq ido-default-file-method 'selected-window)
-  ;;   ;; (setq ido-default-buffer-method 'selected-window)
+  (setq ido-default-file-method 'selected-window)
+  (setq ido-default-buffer-method 'selected-window)
   ;;   ;; ;; Use the current window for indirect buffer display
-  ;;   ;; (setq org-indirect-buffer-display 'current-window)
+  (setq org-indirect-buffer-display 'current-window)
 
   ;; Set default column view headings: Task Effort Clock_Summary
   (setq org-columns-default-format
